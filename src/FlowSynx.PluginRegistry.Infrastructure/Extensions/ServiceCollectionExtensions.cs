a52dfa414db5bef6b3517635dda896e364a729b4
ziagham <amin.ziagham@outlook.com>
2025-05-30 23:26:47 +0200

    Add NormalizePostgresConnectionString for supporting both URL-based and normal connection string for PostgreSQL #13

--- src/FlowSynx.PluginRegistry.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ src/FlowSynx.PluginRegistry.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -6,6 +6,8 @@ using FlowSynx.PluginRegistry.Infrastructure.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Npgsql;
+using System;
 
 namespace FlowSynx.PluginRegistry.Infrastructure.Extensions;
 
@@ -13,7 +15,8 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddPostgresPersistenceLayer(this IServiceCollection services, IConfiguration configuration)
     {
-        var connectionString = configuration.GetConnectionString("DatabaseConnection"); 
+        var connectionString = configuration.GetConnectionString("DatabaseConnection");
+        var normalizePostgresConnectionString = NormalizePostgresConnectionString(connectionString);
 
         services
             .AddSingleton<ISystemClock, SystemClock>()
@@ -23,8 +26,37 @@ public static class ServiceCollectionExtensions
             .AddScoped<IGravatar, Gravatar>()
             .AddDbContextFactory<ApplicationContext>(options =>
             {
-                options.UseNpgsql(connectionString);
+                options.UseNpgsql(normalizePostgresConnectionString);
             });
         return services;
+    }
+
+    public static string NormalizePostgresConnectionString(string? input)
+    {
+        if (string.IsNullOrWhiteSpace(input))
+            throw new Exception("Database connectionstring is required!");
+
+        if (input.StartsWith("postgres://") || input.StartsWith("postgresql://"))
+        {
+            var uri = new Uri(input);
+            var userInfo = uri.UserInfo.Split(':', 2);
+            var username = userInfo[0];
+            var password = userInfo.Length > 1 ? userInfo[1] : "";
+            var host = uri.Host;
+            var port = uri.Port == -1 ? 5432 : uri.Port;
+
+            return new NpgsqlConnectionStringBuilder
+            {
+                Host = host,
+                Port = port,
+                Username = username,
+                Password = password,
+                Database = uri.AbsolutePath.TrimStart('/'),
+                SslMode = SslMode.Require,
+                TrustServerCertificate = true
+            }.ConnectionString;
+        }
+
+        return input;
     }
 }
